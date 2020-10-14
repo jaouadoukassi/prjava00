ae479eda16de76d4715a9f6f3a0cd6f819d0f636
jaouad oukassi <jaouadoukassi1@gmail.com>
2020-10-15 00:11:27 +0200

    https://github.com/jaouadoukassi/prjava00.git

---
+++ prjava00.java
@@ -0,0 +1,7 @@
+class prjava00 {
+    public static void main(String[] args) {
+        System.out.println("Hola mon");
+        System.out.println("Versió 0.1 del projecte prjava00");
+        }
+        
+}
